c793626e94347dc258db7986cd573fffe49e19e9
SavithaKiran <aadsaki@gmail.com>
2019-02-07 13:53:41 +0530

    cg

--- testy/src/feb1/Arraydup.java
+++ testy/src/feb1/Arraydup.java
@@ -6,7 +6,7 @@ public class Arraydup {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String[] arr= {"I","am","what","I","am"};
+		String[] arr= {"I","am","what","I","am"};//demo testing
 		TreeSet<String> unique = new TreeSet<>();
 		System.out.println("Duplicate Strings are ");
 		for(String dups:arr) 
